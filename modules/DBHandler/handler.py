4c0c1a0bf06f5d7261d197437b83e9e5a947a72c
id3ntity99 <jas06113@gmail.com>
2022-01-16 11:08:08 +0900

    feat: with문 magic method 추가

--- modules/DBHandler/handler.py
+++ modules/DBHandler/handler.py
@@ -3,6 +3,20 @@ import json
 from sqlalchemy.orm import sessionmaker
 
 
+class SessionManager(object):
+    def __init__(self, engine):
+        self.engine = engine
+        self.session = None
+
+    def __enter__(self):
+        Session = sessionmaker()
+        self.session = Session(bind=self.engine)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
+
+
 class MyDatabase:
     def connect_db(self, uri) -> None:
         self.engine = db.create_engine("sqlite:///{0}".format(uri))
@@ -31,22 +45,23 @@ class MyDatabase:
 
     def select_where_name_is(self, Model, keyword: str):
         query_keyword = "%{0}%".format(keyword)
-        Session = sessionmaker(bind=self.engine)
-        # 나중에 with 문으로 바꿀 것.
-        session = Session()
-        result = session.query(Model).filter(Model.name.like(query_keyword)).all()
+        manager = SessionManager(self.engine)
+        with manager:
+            result = (
+                manager.session.query(Model)
+                .filter(Model.name.like(query_keyword))
+                .all()
+            )
+        # 데이터베이스 검색 결과가 없으면 아래 메시지를 리턴
         if not result:
             err_msg = {"success": False, "message": "요청하신 데이터를 찾을 수 없습니다"}
             return json.dumps(err_msg)
-        session.close()
         json_data = self._jsonify(result)
         return json_data
 
     def select_all(self, Model) -> list:
-        Session = sessionmaker(bind=self.engine)
-        # 나중에 with 문으로 바꿀 것.
-        session = Session()
-        result = session.query(Model).all()
-        session.close()
+        manager = SessionManager(self.engine)
+        with manager:
+            result = manager.session.query(Model).all()
         json_data = self._jsonify(result)
         return json_data
