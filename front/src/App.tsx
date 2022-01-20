96f138bb221fda8710a9339edcd18b7ee25641dc
LeeJaeHoon <dlwogns3413@naver.com>
2022-01-20 19:42:54 +0900

    feat: clear Header

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,6 +3,8 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global-style";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "./styles/theme";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +13,11 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
         <ReactQueryDevtools initialIsOpen={true} />
       </ThemeProvider>
     </QueryClientProvider>
